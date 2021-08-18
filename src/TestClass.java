c6d62fa008254f9d0f20ae9904eaa82792f2c61f
ahmetilkerdinc <89147854+ahmetilkerdinc@users.noreply.github.com>
2021-08-18 18:54:13 +0300

    Update TestClass.java

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,7 +1,7 @@
 public class TestClass {
     public static void main(String[] args) {
         System.out.println("Git ten merhaba!");
-
+        System.out.println("Github dan merhaba!");
 
     }
 }
